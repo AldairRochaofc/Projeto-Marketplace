a7e8df95f7f6a75164dbb3a6704370a86e0b9737
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2023-12-22 01:57:19 -0300

    creating files for shop system

---
+++ scripts/ShopSystem/Shop.js
@@ -0,0 +1,3 @@
+import { data } from "./ShopDataBase/fetchDataBase.js";
+
+//On this Java Script Code we will Generate page based on the shop data base. We also need to create an cart system e make it comunicates with each other.
