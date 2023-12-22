68b3446e5d3dcd0d4bd10fc35ba520ccfa8e94cc
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2023-12-22 02:23:20 -0300

    Creating files for shoping System

--- scripts/ShopSystem/Shop.js
+++ scripts/ShopSystem/Shop.js
@@ -1,3 +1,4 @@
-import { data } from "./ShopDataBase/fetchDataBase.js";
+import {/*UserID and UserHistory and BrowserHistory in case there isn't a logged account*/ } from "../LandingPage";
 
 //On this Java Script Code we will Generate page based on the shop data base. We also need to create an cart system e make it comunicates with each other.
+//utilize fetch para fazer a comunicação com o JSON FILE. mas também podemos utilizar servidores git para providenciar um código mais limpo
